60e0981c09c237f90d0f3ff834356559b4a199b9
Adrian E <addisaden@gmail.com>
2013-09-15 09:20:40 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myse::Application.config.secret_key_base = '52342ad8530dc3b108d389eb5bc362a38cf748660f55d6bc7224d880195326d9d114d61b992acee37daaa59a56120001493bd4d3add874d36ca0ff495888ac5b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Myse::Application.config.session_store :cookie_store, key: '_myse_session'
