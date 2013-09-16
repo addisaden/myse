9083fcb819e9652702b11724815b36294290169d
Adrian E <addisaden@gmail.com>
2013-09-16 19:56:08 +0200

    added some validations for searchengine - and an argument counter

--- app/models/searchengine.rb
+++ app/models/searchengine.rb
@@ -1,2 +1,12 @@
 class Searchengine < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validates :url, presence: true, format: { with:  /\Ahttps?:\/\/\S+\z/i, message: "Only valid urls allowed." }
+
+  before_save :count_arguments
+
+  private
+
+  def count_arguments
+    self.arg_count = self.url.count('%s')/2
+  end
 end
